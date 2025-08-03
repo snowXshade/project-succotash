dcadf04ebc18e5aace8f329160b17d0f1c0144dd
Ruchi Sharma <112816398+snowXshade@users.noreply.github.com>
2025-08-03 15:28:41 +0530

    Update pro.js

--- src/compo/pro.js
+++ src/compo/pro.js
@@ -4,21 +4,21 @@ export const projects = [
     description: 'A full-featured dashboard allowing admin to add and manage student data in the database.',
     tech: ['React', 'MongoDB', 'Express'],
     url :'students',
-    link: 'https://github.com/snowXshade/project-succotash/blob/main/front/front/src/pages/studentDashBoard.jsx'
+    link: 'https://github.com/snowXshade/project-succotash/blob/main/src/pages/studentDashBoard.jsx'
   },
   {
     title: 'To-Do App with Expense Calculator',
     description: 'A combined productivity app where users can log in, manage their daily tasks, and calculate expenses.',
     tech: ['React', 'CSS', 'JavaScript'],
     url :'todo',
-    link: 'https://github.com/snowXshade/project-succotash/blob/main/front/front/src/pages/Todo.jsx'
+    link: 'https://github.com/snowXshade/project-succotash/blob/main/src/pages/Todo.jsx'
   },
   {
     title: 'Weight Tracker with Graphs',
     description: 'An interactive tracker that lets users log their weight over time and view trends through Recharts graphs.',
     tech: ['React', 'Recharts'],
     url :'wgt',
-    link: 'https://github.com/snowXshade/project-succotash/blob/main/front/front/src/pages/WeightTracker.jsx'
+    link: 'https://github.com/snowXshade/project-succotash/blob/main/src/pages/WeightTracker.jsx'
   },
 ];
 
